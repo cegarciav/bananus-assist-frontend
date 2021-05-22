aef77d2fdef5081d9d228bb69c9ddc9d60b22d30
Kelsey Franken <kcfranken@uc.cl>
2021-05-22 17:58:39 -0400

    Improvement information view: figma design

--- src/components/Information/information.js
+++ src/components/Information/information.js
@@ -19,6 +19,7 @@ import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Paper from '@material-ui/core/Paper';
 
 
 
@@ -66,28 +67,52 @@ const qas = [
   },
 ];
 
+const steps = [
+  {
+    title: '1.',
+    description: "En cada uno de los dispositivos de la tienda se encuentra disponible el catálogo de productos en venta.",
+    img: '/media/images/how-1.png'
+
+  },
+  {
+    title: '2.',
+    description: "Al momento de requerir asistencia, el cliente podrá solicitarla por medio del botón en la barra e navegación que dice “Solicitar asistencia”",
+    img: './media/images/how-2.png'
+  },
+  {
+    title: '3.',
+    description: 'En caso de haber asistentes disponibles, se desplegará un mensaje para que el cliente se acerque al tablet central, en donde se podrá contactar por medio de una videollamada con el asistente y resolver sus consultas.',
+    img: './media/images/how-3.png'
+  },
+  {
+    title: '4.',
+    description: "Si no hay asistentes disponibles, se desplegará un mensaje informandole al cliente. Este podrá continuar con su proceso de compra y cuando se desocupe el asistente solicitar que le asista.",
+    img: './media/images/how-4.png'
+  },
+];
+
 
 export default function Information() {
   const classes = useStyles();
 
   return (
     <React.Fragment>
+      
       <CssBaseline />
+      <head> 
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"/>
+      </head>
   
-      <Container maxWidth="lg"  component ="Title" lassName={classes.titleContent}>
+      <Container maxWidth="lg"  component ="Title" >
         <Typography variant="h3" align = "left" className={classes.infoTitle}>
-
-        <InfoOutlinedIcon fontSize = "large" align = "left" className={classes.infoTitle}/>
-
+        <InfoOutlinedIcon fontSize = "large" align = "center" />
           Informaciones
         </Typography>
-
-        
       </Container>
 
       <Container maxWidth="lg" component="description" >
   
-      <Typography align= "justify" variant = "subtitle1" className={classes.infoDescription}>
+      <Typography align= "justify" variant="body1" className={classes.infoDescription}>
       Bananus assist es una plataforma que permite ofrecer asistencia y acompañar a los 
       clientes de manera remota en su proceso de compra. 
       De esta manera los clientes pueden comprar de manera segura minimizando el contacto físico.
@@ -103,42 +128,34 @@ export default function Information() {
         
       </Container>
 
-      <Divider />
+      
 
-  <List component="nav"  aria-label="how-steps">
-  <ListItem button>
-    <ListItemText 
-    primary="1" 
-    secondary="En cada uno de los dispositivos de la tienda se encuentra disponible el catálogo de productos 
-    en venta."
-    />
-  </ListItem>
-  <Divider />
-  <ListItem button>
-    <ListItemText 
-    primary="2" 
-    secondary="Al momento de requerir asistencia, el cliente podrá solicitarla por medio del botón en la barra e navegación que dice “Solicitar asistencia”"
-    />
-  </ListItem>
-  <Divider />
-  <ListItem button>
-    <ListItemText 
-    primary="3" 
-    secondary="En caso de haber asistentes disponibles, se desplegará un mensaje para que el 
-    cliente se acerque al tablet central, en donde se podrá contactar por medio de una videollamada con 
-    el asistente y resolver sus consultas."
-    />
-  </ListItem>
-  <Divider  />
-  <ListItem button>
-    <ListItemText 
-    primary="4" 
-    secondary="Si no hay asistentes disponibles, se desplegará un mensaje informandole al cliente. 
-    Este podrá continuar con su proceso de compra y cuando se desocupe el asistente solicitar que le asista."
-    />
-  </ListItem>
-  <Divider  />
-</List>
+
+      <Container  component="steps" >
+      <Divider className={classes.dividerLine}/>
+      {steps.map((step) => (
+        <div>
+        <Container maxWidth="lg" component="step" >
+        <Typography align= "justify" variant = "h5" className={classes.stepTitle}>
+        {step.title}
+          </Typography>
+          <Typography align= "justify" variant = "body1" className={classes.stepDescription}>
+        {step.description}
+          </Typography>    
+
+          <Paper variant="outlined">
+        <img alt = "" src={step.img} />
+            </Paper>      
+          
+          </Container>
+          <Divider />
+          </div>
+
+         ))}
+  
+      
+        
+      </Container>
 
 <div className={classes.root}>
       <Accordion>
@@ -152,14 +169,13 @@ export default function Information() {
         <AccordionDetails>
         <Container maxWidth="lg" component="main">
         <Grid container spacing={5} alignItems="flex-end">
-          {qas.map((tier) => (
+      {qas.map((tier) => (
             
-            <Grid item  xs={12} sm={6} md={4}>
+    <Grid item  xs={12} sm={6} md={4}>
              
-
     <Card className={classes.root}>
-      
         <CardMedia
+        
           component="img"
           height="110"
           image={tier.img}
--- src/components/Information/styles-information.js
+++ src/components/Information/styles-information.js
@@ -4,33 +4,71 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 const useStyles = makeStyles((theme) => ({
-  '@global': {
-    ul: {
-      margin: 0,
-      padding: 0,
-      listStyle: 'none',
-      
-    },
-  },
-
   titleContent: {
-    padding: theme.spacing(40,40,40,34),
-    
+    padding: theme.spacing(4,0,0,0),
   },
 
   infoTitle: {
-    color: '#195E6D',
+    color: '#356B77',
+    padding: theme.spacing(4,0,0,0),
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: 600,
+    fontSize: '34px',
+    lineHeight: '41px',
+    display: 'flex',
+    alignItems: 'center',
   },
 
   infoDescription: {
-    color: '#878787',
-    padding: theme.spacing(0,0,3,0),
+    color: '#000000',
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '24px',
+    lineHeight: '29px',
+    display: 'flex',
+    alignItems: 'center',
+    width: '1092px',
+    height: '122px',
+    left: '149px',
+    top: '296px',
   },
 
   infoHow: {
-    color: '000000',
-    padding: theme.spacing(0,0,3,0),
+    color: '#000000',
+    padding: theme.spacing(3,0,0,3),
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: 600,
+    fontSize: '24px',
+    lineHeight: '29px',
+    display: 'flex',
+    alignItems: 'center',
   },
+
+  stepTitle: {
+    color: '#356B77',
+    padding: theme.spacing(4,0,0,0),
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: 500,
+    fontSize: '57px',
+    lineHeight: '69px',
+    display: 'flex',
+    alignItems: 'center',
+    width: '63px',
+    height: '40px',
+    left: '204px',
+    top: '685px',
+  },
+  stepDescription: {
+    color: '#000000',
+    padding: theme.spacing(3,0,0,3),
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+  },
+
 
 
   cardHeader: {
